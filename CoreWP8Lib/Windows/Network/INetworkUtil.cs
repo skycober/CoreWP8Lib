2ea17b3011a373e361a8e682d70fd5d519484c5d
Meng xinbin <mengxinbin@live.com>
2013-05-06 11:46:20 +0800

    Network Util

---
+++ CoreWP8Lib/Windows/Network/INetworkUtil.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreWP8Lib.Windows.Network
+{
+    /// <summary>
+    /// 网络状态查询接口类
+    /// </summary>
+    interface INetworkUtil
+    {
+        /// <summary>
+        /// 当前是否连接了网络
+        /// </summary>
+        /// <returns></returns>
+        public bool IsExistNetwork();
+        /// <summary>
+        /// 当前是否处于WiFi网络
+        /// </summary>
+        /// <returns></returns>
+        public bool IsWifiNetwork();
+    }
+}
---
+++ CoreWP8Lib/Windows/Network/NetworkUtil.cs
@@ -0,0 +1,36 @@
+﻿using CoreWP8Lib.Windows.Network;
+using Microsoft.Phone.Net.NetworkInformation;
+using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace CoreWP8Lib.Windows.Network
+{
+    public class NetworkUtil : INetworkUtil
+    {
+        private static NetworkUtil util;
+        public NetworkUtil CreateInstance()
+        {
+            if (null == util) util = new NetworkUtil();
+            return util;
+        }
+
+        public bool IsExistNetwork()
+        {
+            return NetworkInterface.GetIsNetworkAvailable();
+        }
+
+        public bool IsWifiNetwork()
+        {
+            NetworkInterfaceType type = NetworkInterface.NetworkInterfaceType;
+            return type == NetworkInterfaceType.Wireless80211;
+        }
+    }
+}
